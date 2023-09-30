e6c185767d7bb540fbe4788eb6c40ecd0192dc48
yotsuha <yoshioka.yotsuha.infratop@gmail.com>
2023-09-30 13:33:38 +0900

    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,19 @@
 class FavoritesController < ApplicationController
   def create
-    # if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if !Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
       book = Book.find(params[:book_id])
       @favorite = current_user.favorites.new(book_id: book.id)
       @favorite.save
-      redirect_to request.referer
-      # render "favorites/replace_btn.js.erb"
-    # end
+      render "favorites/replace_btn.js.erb"
+    end
   end
 
   def destroy
-    # if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
       book = Book.find(params[:book_id])
       @favorite = current_user.favorites.find_by(book_id: book.id)
       @favorite.destroy
-      redirect_to request.referer
-      # render "favorites/replace_btn.js.erb"
-    # end
+      render "favorites/replace_btn.js.erb"
+    end
   end
 end
